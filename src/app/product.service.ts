93487012b3be539c22f3c9a06b58c5ab7422f24d
Leonel Elimpe <leonelngande@gmail.com>
2019-11-04 22:01:29 +0100

    Import Response class, render dynamic album release date

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -11,7 +11,7 @@ export class ProductService {
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl)
-      .map(response => response.json());
+      .map((response: Response) => response.json());
   }
 
 }
