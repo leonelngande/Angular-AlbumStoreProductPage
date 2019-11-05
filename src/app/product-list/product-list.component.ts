e45b1a794ac248d87b439f864a4f49c3e26d70f3
Leonel Elimpe <leonelngande@gmail.com>
2019-11-05 13:29:14 +0100

    Add navigation with angular router

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../product.service';
 import {Product} from '../product';
 
@@ -11,7 +11,8 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private _productService: ProductService) { }
+  constructor(private _productService: ProductService) {
+  }
 
   ngOnInit() {
     this._productService.getProducts()
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
-import { Album } from './album';
+import {Observable} from 'rxjs/Observable';
+import {Album} from './album';
 import {Product} from './product';
 
 @Injectable()
@@ -11,7 +11,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   private _productsUrl = '../assets/products.json';
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http) {
+  }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
